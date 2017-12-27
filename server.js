4580ae989052bf26a0dd61024536ddaac58b2f05
Frank LaNasa <frank@Franks-MBP.fios-router.home>
2017-12-27 17:47:24 -0500

    cors for all origins for now

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ require('./server/config/config.js');
 
 let express = require('express');
 let app = express();
-
+let cors = require('cors');
 let apiRoutes = require('./server/routes/apiRoutes.js');
 
+app.use(cors());
 app.use('/', apiRoutes);
 
 let server = app.listen(process.env.PORT, () => {
